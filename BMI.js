6d8939fd6922cb08e62020fe06e5e005a3ae2d54
ALEXANDERkETERLING <alexander_keterling@teilnehmer.cdemy.de>
2024-07-31 10:25:14 +0200

    Updatesexage

--- BMI.js
+++ BMI.js
@@ -42,6 +42,48 @@ function getBMIStatus(bmi, category) {
         } else if (bmi >= 25.0) {
             status = '<p>Obese</p>';
         }
+
+    } else if (category === "female") {
+        if (bmi < 15.0) {
+            status = '<p>Severely Underweight</p>';
+        } else if (bmi >= 15.0 && bmi <= 17.4) {
+            status = '<p>Underweight</p>';
+        } else if (bmi >= 19.1 && bmi <= 23.0) {
+            status = '<p>Normal</p>';
+        } else if (bmi >= 24.0 && bmi <= 26.9) {
+            status = '<p>Overweight</p>';
+        } else if (bmi >= 26.0 && bmi <= 34.9) {
+            status = '<p>Moderately Obese</p>';
+        } else if (bmi >= 34.0 && bmi <= 38.9) {
+            status = '<p>Severely Obese</p>';
+        } else if (bmi >= 39.0) {
+            status = '<p>Morbidly Obese</p>';
+        }
+
+    } else if (category === "male") {
+        
+            if (bmi < 16.0) {
+                status = '<p>Severely Underweight</p>';
+            } else if (bmi >= 16.0 && bmi <= 18.4) {
+                status = '<p>Underweight</p>';
+            } else if (bmi >= 18.5 && bmi <= 24.9) {
+                status = '<p>Normal</p>';
+            } else if (bmi >= 25.0 && bmi <= 29.9) {
+                status = '<p>Overweight</p>';
+            } else if (bmi >= 30.0 && bmi <= 34.9) {
+                status = '<p>Moderately Obese</p>';
+            } else if (bmi >= 35.0 && bmi <= 39.9) {
+                status = '<p>Severely Obese</p>';
+            } else if (bmi >= 40.0) {
+                status = '<p>Morbidly Obese</p>';
+            }
+
+
+
+
+
+
+
     }
     return status;
 }
